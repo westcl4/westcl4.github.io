59353b667bdbf4aef6585f0166fdcb636c54cf47
clwsham <53279484+clwsham@users.noreply.github.com>
2020-04-30 00:54:44 +1200

    Added images and profile bar

--- assets/js/sitejs.js
+++ assets/js/sitejs.js
@@ -1,1 +1,17 @@
+fetch("assets/js/data.json")
+.then(response => { return response.json()})
+.then(data =>{
+    let music = data["data"]
+    let artists = document.getElementsByClassName
 
+})
+
+function topTenTracks(data){
+    if (typeof(data) == 'object') {
+        document.write('ol');
+
+        for (var i in data){
+            
+        }
+    }
+}
