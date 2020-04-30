a275911c9061259bd822103c623a2e94357ade3b
clwsham <53279484+clwsham@users.noreply.github.com>
2020-04-30 18:03:30 +1200

    split json into 2

--- assets/js/sitejs.js
+++ assets/js/sitejs.js
@@ -1,17 +1,53 @@
 
-let data = fetch("../assets/js/data.json")
+ 
+fetch("../assets/js/artists.json")
+ 
 
-    .then(res => {
-        return res.json();
+    .then(artistsResponse => {
+        return artistsResponse.json();
     })
 
-    .then(data => {
-        console.log(data["Data"])
+    .then (artistData => {
+        console.log(artistData["Artists"])
+        artistData["Artists"].forEach(Artist => {
+            
+        });
+    })
+
+        
+fetch("../assets/js/songs.json")
+    
+    .then(songsResponse =>{
+        return songsResponse.json();
+    })
+    
+    .then(songdata => {
+        console.log(songdata["Songs"])
+    })
+    
+   
+
+    /*data ["img"][0].forEach(Img =>{
+
     });
 
+    for (let i = 0; i < "Data"[0]["Artists"]; i++) {
+        Artists[i].src = (data["Data"][0]["Artist"][i]["img"][i]);
+    
+    }
+    
+    for (let i = 0; i < "Data"[0]["Songs"]; i++) {
+        Songs[i].src = (data["Data"][0]["Artist"][i]["Song"][i]["Scrobbles"][i]);
+    
+    
+    }
+    for (let i = 0; i < data.length; i++) {
+        musicdata = data[i];
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(musicdata.))
+    }
 
-
-for (let i = 0; i < "Data"[0]["Artists"]; i++) {
+/*for (let i = 0; i < "Data"[0]["Artists"]; i++) {
     Artists[i].src = (data["Data"][0]["Artist"][i]["img"][i]);
 
 }
@@ -20,4 +56,4 @@ for (let i = 0; i < "Data"[0]["Songs"]; i++) {
     Songs[i].src = (data["Data"][0]["Artist"][i]["Song"][i]["Scrobbles"]);
 
 
-}
+}*/
